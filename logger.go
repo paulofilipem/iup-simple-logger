74d35c57aa00993cba5363c3bb8b363a41429159
Paulo  Filipe <paulofilipe@MacBook-Pro-de-Paulo.local>
2018-02-13 23:01:21 -0200

    code comments

--- logger.go
+++ logger.go
@@ -1,65 +1,78 @@
 package logger
 
 import (
-  "flag"
-  "log"
+	"flag"
+	"log"
 	"os"
 )
 
 var (
-	Log     *log.Logger
-  logPath = "application.log"
+	//Log is a golang log handle
+	Log *log.Logger
+
+	//Log path full or filename (If u want to write file in the runtime folder)
+	logPath = "application.log"
 )
 
+//setFileLog set log path full or filename (If u want to write file in the runtime folder)
 func setFileLog(filename string) {
-  logPath = filename
+	logPath = filename
 }
 
+//getFileLog get log path or filename
 func getFileLog() string {
-  return logPath
+	return logPath
 }
 
 func init() {
 
-   flag.Parse()
+	flag.Parse()
 	var file, err1 = os.OpenFile(logPath, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0660)
 
-   if err1 != nil {
-      panic(err1)
-   }
-   
-   Log = log.New(file, "", log.LstdFlags)
-   Log.Println("LogFile : " + logPath)
+	if err1 != nil {
+		panic(err1)
+	}
+
+	Log = log.New(file, "", log.LstdFlags)
+	Log.Println("LogFile : " + logPath)
 }
 
+//Debug func
 func Debug(message string) {
-    Log.Println("DEBUG: " + message)
+	Log.Println("DEBUG: " + message)
 }
 
+//Info func
 func Info(message string) {
-    Log.Println("INFO: " + message)
+	Log.Println("INFO: " + message)
 }
 
-func Notice(message string){
-    Log.Println("NOTICE: " + message)
+//Notice func
+func Notice(message string) {
+	Log.Println("NOTICE: " + message)
 }
 
-func Warning(message string){
-    Log.Println("WARNING: " + message)
+//Warning func
+func Warning(message string) {
+	Log.Println("WARNING: " + message)
 }
 
+//Error func
 func Error(message string) {
-    Log.Println("ERROR: " + message)
+	Log.Println("ERROR: " + message)
 }
 
-func Critical(message string){
-    Log.Println("CRITICAL: " + message)
+//Critical func
+func Critical(message string) {
+	Log.Println("CRITICAL: " + message)
 }
 
-func Alert(message string){
-    Log.Println("ALERT: " + message)
+//Alert func
+func Alert(message string) {
+	Log.Println("ALERT: " + message)
 }
 
-func Emergency(message string){
-    Log.Println("EMERGENCY: " + message)
+//Emergency func
+func Emergency(message string) {
+	Log.Println("EMERGENCY: " + message)
 }
